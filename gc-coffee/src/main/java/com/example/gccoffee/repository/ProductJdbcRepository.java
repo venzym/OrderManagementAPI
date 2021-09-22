58fa7ebbc45467a151d607cb1ef84c9bddb3e1ef
venzym <venzym@naver.com>
2021-09-22 22:19:05 +0900

    feat: repository 추가

--- gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
+++ gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -1,2 +1,69 @@
-package com.example.gccoffee.repository;public class ProductJdbcRepository {
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.example.gccoffee.repository.JdbcUtils.toLocalDateTime;
+import static com.example.gccoffee.repository.JdbcUtils.toUUID;
+
+public class ProductJdbcRepository implements ProductRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public ProductJdbcRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.jdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return jdbcTemplate.query("select * from products", productRowMapper);
+    }
+
+    @Override
+    public Product insert(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product update(Product update) {
+        return null;
+    }
+
+    @Override
+    public Optional<Product> findById(UUID productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> findByName(String productName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Product> findByCategoryName(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    private static final RowMapper<Product> productRowMapper = (resultSet, i) -> {
+        UUID productId = toUUID(resultSet.getBytes("product_id"));
+        String productName = resultSet.getString("product_name");
+        Category category = Category.valueOf(resultSet.getString("category"));
+        Long price = resultSet.getLong("price");
+        String description = resultSet.getString("description");
+        LocalDateTime createdAt = toLocalDateTime(resultSet.getTimestamp("createdAt"));
+        LocalDateTime updatedAt = toLocalDateTime(resultSet.getTimestamp("updatedAt"));
+        return new Product(productId, productName, category, price, description, createdAt, updatedAt);
+    };
 }
--- gc-coffee/src/main/java/com/example/gccoffee/repository/ProductRepository.java
+++ gc-coffee/src/main/java/com/example/gccoffee/repository/ProductRepository.java
@@ -1,2 +1,24 @@
-package com.example.gccoffee.repository;public interface ProductRepository {
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProductRepository {
+    List<Product> findAll();
+
+    Product insert(Product product);
+
+    Product update(Product update);
+
+    Optional<Product> findById(UUID productId);
+
+    Optional<Product> findByName(String productName);
+
+    List<Product> findByCategoryName(Category category);
+
+    void deleteAll();
 }
