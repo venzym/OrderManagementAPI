5e0a58f62793c7741a5cf88fc8fd0a08f8e103e3
venzym <venzym@naver.com>
2021-09-22 22:18:25 +0900

    feat: model 추가

---
+++ gc-coffee/src/main/java/com/example/gccoffee/model/Product.java
@@ -0,0 +1,81 @@
+package com.example.gccoffee.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Product {
+    private final UUID uuid;
+    private String productName;
+    private Category category;
+    private long price;
+    private String description;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt; // set 할때마다 변경
+
+    public Product(UUID uuid, String productName, Category category, long price) {
+        this.uuid = uuid;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Product(UUID uuid, String productName, Category category, long price, String description, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.uuid = uuid;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
