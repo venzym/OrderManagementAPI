f86fa52ea985fbbe32e5301811b4fa6711a56f3c
venzym <venzym@naver.com>
2021-09-23 23:38:34 +0900

    feat: controller 추가

--- gc-coffee/src/main/java/com/example/gccoffee/controller/CreateProductRequest.java
+++ gc-coffee/src/main/java/com/example/gccoffee/controller/CreateProductRequest.java
@@ -1,2 +1,27 @@
-package com.example.gccoffee.controller;public class CreateProductRequest {
+package com.example.gccoffee.controller;
+
+import com.example.gccoffee.model.Category;
+
+public class CreateProductRequest {
+    private String productName;
+    private Category category;
+    private long price;
+    private String description;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
