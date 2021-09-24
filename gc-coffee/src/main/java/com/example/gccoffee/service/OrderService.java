f032501339a0c0a6266a6d2e59d6c517faa303b1
venzym <venzym@naver.com>
2021-09-25 00:01:46 +0900

    feat: order service 추가

--- gc-coffee/src/main/java/com/example/gccoffee/service/OrderService.java
+++ gc-coffee/src/main/java/com/example/gccoffee/service/OrderService.java
@@ -1,2 +1,11 @@
-package com.example.gccoffee.service;public interface OrderService {
+package com.example.gccoffee.service;
+
+import com.example.gccoffee.model.Email;
+import com.example.gccoffee.model.Order;
+import com.example.gccoffee.model.OrderItem;
+
+import java.util.List;
+
+public interface OrderService {
+    Order createOrder(Email email, String address, String postcode, List<OrderItem> orderItems);
 }
